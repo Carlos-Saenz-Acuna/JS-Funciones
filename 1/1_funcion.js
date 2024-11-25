c42e8d19d7dede310affc0ba0116b52d84d9e84e
Carlos-Saenz-Acuna <carlos.ignacio.saenz@gmail.com>
2024-11-24 21:56:42 -0300

    Ejercicio 1 listo

---
+++ 1/1_funcion.js
@@ -0,0 +1,9 @@
+//Funcion orginal
+function example(a, b, c) {
+return a + b + c;
+}
+
+//Funcion de expresion
+const example = function (a, b, c) {
+  return a + b + c;
+};
---
+++ 2/2_arrow.js
@@ -0,0 +1,12 @@
+// Funcion original
+
+let suma = function (a, b) {
+  return a + b;
+};
+
+
+// Funcion Arrow
+
+const suma = (a, b) => a + b;
+
+
