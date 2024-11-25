7868d138f532f90ca5df676c61c78ab31e31ac66
Carlos-Saenz-Acuna <carlos.ignacio.saenz@gmail.com>
2024-11-24 23:29:45 -0300

    Ejercicio 3 colores a negro listo

---
+++ 3/assets/js/main.js
@@ -0,0 +1,20 @@
+const colorAzul = document.querySelector("#azul");
+const colorRojo = document.querySelector("#rojo");
+const colorVerde = document.querySelector("#verde");
+const colorAmarillo = document.querySelector("#amarillo");
+
+colorAzul.addEventListener("click", function () {
+  colorAzul.style.backgroundColor = "black"; 
+});
+
+colorRojo.addEventListener("click", function () {
+  colorRojo.style.backgroundColor = "black"; 
+});
+
+colorVerde.addEventListener("click", function () {
+  colorVerde.style.backgroundColor = "black"; 
+});
+
+colorAmarillo.addEventListener("click", function () {
+  colorAmarillo.style.backgroundColor = "black"; 
+});
